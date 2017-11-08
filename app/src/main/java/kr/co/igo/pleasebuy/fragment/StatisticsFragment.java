c2fb7ea2d7960948e96def274f840b63843d1d24
baekseunghyun <backseunghyun>
2017-11-09 01:00:46 +0900

    ..

--- app/src/main/java/kr/co/igo/pleasebuy/fragment/StatisticsFragment.java
+++ app/src/main/java/kr/co/igo/pleasebuy/fragment/StatisticsFragment.java
@@ -1,8 +1,12 @@
 package kr.co.igo.pleasebuy.fragment;
 
 import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +19,14 @@ import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.Utils;
 import com.loopj.android.http.RequestParams;
 
 import org.json.JSONArray;
@@ -118,8 +130,8 @@ public class StatisticsFragment extends BaseFragment {
         lc_chart1.setTouchEnabled(true);
 
         // enable scaling and dragging
-        lc_chart1.setDragEnabled(true);
-        lc_chart1.setScaleEnabled(true);
+        lc_chart1.setDragEnabled(false);
+        lc_chart1.setScaleEnabled(false);
 
         // if disabled, scaling can be done on x- and y-axis separately
         lc_chart1.setPinchZoom(false);
@@ -129,7 +141,128 @@ public class StatisticsFragment extends BaseFragment {
         l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
         l.setOrientation(Legend.LegendOrientation.VERTICAL);
         l.setDrawInside(false);
+
+        // x-axis limit line
+        LimitLine llXAxis = new LimitLine(10f, "");
+        llXAxis.setLineWidth(0.6f);
+        llXAxis.enableDashedLine(10f, 10f, 0f);
+        llXAxis.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_BOTTOM);
+        llXAxis.setTextSize(10f);
+        llXAxis.setLineColor(getResources().getColor(R.color.c_cecece));
+
+        XAxis xAxis = lc_chart1.getXAxis();
+//        xAxis.enableGridDashedLine(10f, 10f, 0f);
+        xAxis.addLimitLine(llXAxis);
+
+        LimitLine ll1 = new LimitLine(150f, "Upper Limit");
+        ll1.setLineWidth(4f);
+        ll1.enableDashedLine(10f, 10f, 0f);
+        ll1.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_TOP);
+        ll1.setTextSize(10f);
+
+        LimitLine ll2 = new LimitLine(-30f, "Lower Limit");
+        ll2.setLineWidth(4f);
+        ll2.enableDashedLine(10f, 10f, 0f);
+        ll2.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_BOTTOM);
+        ll2.setTextSize(10f);
+
+
+        YAxis leftAxis = lc_chart1.getAxisLeft();
+        leftAxis.removeAllLimitLines(); // reset all limit lines to avoid overlapping lines
+        leftAxis.addLimitLine(ll1);
+//        leftAxis.addLimitLine(ll2);
+        leftAxis.setAxisMaximum(200f);
+        leftAxis.setAxisMinimum(-50f);
+        //leftAxis.setYOffset(20f);
+        leftAxis.enableGridDashedLine(10f, 10f, 0f);
+        leftAxis.setDrawZeroLine(true);
+
+        // limit lines are drawn behind data (and not on top)
+        leftAxis.setDrawLimitLinesBehindData(true);
+
+        lc_chart1.getAxisRight().setEnabled(false);
+
+        //mChart.getViewPortHandler().setMaximumScaleY(2f);
+        //mChart.getViewPortHandler().setMaximumScaleX(2f);
+
+        // add data
+        setData(31, 100);
+
+//        mChart.setVisibleXRange(20);
+//        mChart.setVisibleYRange(20f, AxisDependency.LEFT);
+//        mChart.centerViewTo(20, 50, AxisDependency.LEFT);
+
+        lc_chart1.animateX(2500);
+        //mChart.invalidate();
+
+        // get the legend (only possible after setting data)
+//        Legend l = lc_chart1.getLegend();
+
+        // modify the legend ...
+        l.setForm(Legend.LegendForm.LINE);
+
+        // // dont forget to refresh the drawing
+        // mChart.invalidate();
     }
+
+
+    private void setData(int count, float range) {
+        ArrayList<Entry> values = new ArrayList<Entry>();
+        for (int i = 0; i < count; i++) {
+            float val = (float) (Math.random() * range) + 3;
+//            values.add(new Entry(i, val, getResources().getDrawable(R.drawable.ic_noti)));
+            values.add(new Entry(i, val));
+        }
+
+        LineDataSet set1;
+
+        if (lc_chart1.getData() != null &&
+                lc_chart1.getData().getDataSetCount() > 0) {
+            set1 = (LineDataSet)lc_chart1.getData().getDataSetByIndex(0);
+            set1.setValues(values);
+            lc_chart1.getData().notifyDataChanged();
+            lc_chart1.notifyDataSetChanged();
+        } else {
+            // create a dataset and give it a type
+//            set1 = new LineDataSet(values, "DataSet 1");
+            set1 = new LineDataSet(values, "");
+
+            set1.setDrawIcons(false);
+
+            // set the line to be drawn like this "- - - - - -"
+//            set1.enableDashedLine(10f, 5f, 0f);
+//            set1.enableDashedHighlightLine(10f, 5f, 0f);
+            set1.setColor(Color.RED);
+            set1.setCircleColor(Color.RED);
+            set1.setLineWidth(1f);
+            set1.setCircleRadius(3f);
+            set1.setDrawCircleHole(true);
+            set1.setValueTextSize(0f);
+            set1.setDrawFilled(true);
+//            set1.setFormLineWidth(1f);
+//            set1.setFormLineDashEffect(new DashPathEffect(new float[]{10f, 5f}, 0f));
+            set1.setFormSize(0.f);
+
+            if (Utils.getSDKInt() >= 18) {
+                // fill drawable only supported on api level 18 and above
+                Drawable drawable = ContextCompat.getDrawable(getActivity(), R.drawable.fade_red);
+                set1.setFillDrawable(drawable);
+            }
+            else {
+                set1.setFillColor(Color.BLACK);
+            }
+
+            ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
+            dataSets.add(set1); // add the datasets
+
+            // create a data object with the datasets
+            LineData data = new LineData(dataSets);
+
+            // set data
+            lc_chart1.setData(data);
+        }
+    }
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -223,6 +356,7 @@ public class StatisticsFragment extends BaseFragment {
     private void selectedCategory(int pos) {
         selectedCategoryClear();
         gl_category.getChildAt(pos).setSelected(true);
+        tv_category.setText(mDataList.get(pos));
     }
 
     private void getData(){
